d7be8cb603a916a31d9268a390b3ecb69aae458d
Jorge <jorgecuadradovelasquez@gmail.com>
2024-04-21 21:05:20 -0500

    initialize velocity with random,clipping the position array to bounds, plotting the grid of the function,adding more functions

--- pso.py
+++ pso.py
@@ -1,19 +1,24 @@
 # is ready to run
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 #particle class
 class Particle:
     #initialize with some predefined values
     def __init__(self, dim:int,bounds:float):
         #initialize the position of the particle with random values
         self.position = np.random.uniform(-bounds,bounds,dim)
+        #limiting the position of the particle to the bounds
         #initialize the velocity of the particle with random values
-        self.velocity = np.zeros(dim)
+        self.velocity = np.random.uniform(-bounds,bounds,dim)
         #initialize the personal best position of the particle with random values
         self.personal_best_position = np.zeros(dim)
         #initialize the personal best value of the particle with random values
         self.personal_best_value = None
         #initialize the value of the particle with random values
         self.value = None
+        #initialize the bounds of the particle
+        self.bounds = bounds
     def evaluate(self,objective_function):
         #evaluate the value of the particle
         self.value = objective_function(*self.position)
@@ -23,7 +28,7 @@ class Particle:
         self.velocity = w*self.velocity + c1*r1*(self.personal_best_position-self.position) + c2*r2*(global_best_position-self.position)
     def update_position(self):
         #update the position of the particle
-        self.position = self.position + self.velocity
+        self.position = np.clip(self.position + self.velocity,-self.bounds,self.bounds)
     def update_personal_best(self,optmimization_method:str="min"):
         #update the personal best position and value of the particle
         if self.personal_best_value == None:
@@ -57,6 +62,8 @@ class Swarm:
         self.global_best_value = None
         self.iterations = iterations
         self.optmimization_method = optimization_method
+        # self.fig, self.ax = plt.subplots()
+        # self.data = []
     def lineal_reduction_inertia(self,iteration:int):
         self.w=(self.w_max-self.w_min)*(self.iterations-iteration)/self.iterations+self.w_min
         
@@ -82,14 +89,28 @@ class Swarm:
             # elif self.global_best_value > particle.personal_best_value:
             #     self.global_best_value = particle.personal_best_value
             #     self.global_best_position = particle.personal_best_position
+
     def optimize(self):
         #optimize the particles
         #initialize the value, personal_best_position and personal_best_value of the particles
+        #points=ax.plot([],[],'o')
+        plt.x_label = "x"
+        plt.y_label = "y"
+        plt.title = "Particle Swarm Optimization"
+        x_0 = np.linspace(-self.bounds,self.bounds,100)
+        x_1 = np.linspace(-self.bounds,self.bounds,100)
+        X_0,X_1 = np.meshgrid(x_0,x_1)
+        Z = self.objective_function(X_0,X_1)
+        plt.contour(X_0,X_1,Z,levels=35,cmap='RdGy')
         for particle in self.particles:
             particle.evaluate(self.objective_function)
             particle.update_personal_best(self.optmimization_method)
             print("Particle value: ",particle.value)
         for i in range(self.iterations):
+            x_positions = []
+            y_positions = []
+            x_y_positions = []
+            plt.clf()
             self.find_global_best()
             for particle in self.particles:
                 #r1,r2 is a np.array with dimension dim with random values between 0 and 1
@@ -100,11 +121,36 @@ class Swarm:
                 particle.evaluate(self.objective_function)
                 particle.update_personal_best()
                 self.lineal_reduction_inertia(i)
-            
-            print("Iteration: ",i, "particles",[x.value for x in self.particles])
-            print("Global best value: ",self.global_best_value)
-            print("Global best position: ",self.global_best_position)
-            print("w: ",self.w)
+                x_positions.append(particle.position[0])
+                y_positions.append(particle.position[1])
+                x_y_positions.append((x_positions,y_positions))
+            if i%1 == 0:
+                plt.text(30,57,"iteration: "+str(i))
+                plt.xlim(-self.bounds,self.bounds)
+                plt.ylim(-self.bounds,self.bounds)
+                plt.contour(X_0,X_1,Z,levels=35,cmap='RdGy')
+                plt.scatter(x_positions,y_positions,s=10,c='b')
+                plt.pause(0.1)
+            if i == self.iterations-1:
+                plt.text(30,57,"iteration: "+str(i))
+                plt.xlim(-self.bounds,self.bounds)
+                plt.ylim(-self.bounds,self.bounds)
+                plt.contour(X_0,X_1,Z,levels=35,cmap='RdGy')
+                plt.scatter(x_positions,y_positions,s=10,c='b' )
+                plt.pause(0.5)
+        print("Global best value: ",self.global_best_value)
+        print("Global best position: ",self.global_best_position)
+        print("w: ",self.w)
+        plt.show()
+
+
+            # print("Iteration: ",i, "particles",[x.value for x in self.particles])
+            # x_positions = [x.position[0] for x in self.particles]
+            # y_positions = [x.position[1] for x in self.particles]
+            #data.append((x_positions,y_positions))
+            # plt.scatter(x_positions,y_positions)
+            # plt.show()
+        
 #sphere function
 def test_function_0(x,y):
     return x**2+y**2         
@@ -127,11 +173,17 @@ def test_function_2(x_0,x_1):
 def test_function_3(x_0,x_1):
     f = 20 + x_0**2 + x_1**2 - 10*(np.cos(2*np.pi*x_0)+np.cos(2*np.pi*x_1))
     return(f)
-
-
+#rosenbrock function
+def test_function_4(x_0,x_1):
+    f = 100*(x_1-x_0**2)**2 + (1-x_0)**2
+    return(f)
+#holder table function
+def test_function_5(x_0,x_1):
+    f = -np.abs(np.sin(x_0)*np.cos(x_1)*np.exp(np.abs(1-np.sqrt(x_0**2+x_1**2)/np.pi)))
+    return(f)
 
 # here is just to change the function to test the code 
-swarm1 = Swarm(objective_function=test_function_3,iterations=50,)
+swarm1 = Swarm(objective_function=test_function_1,iterations=100,n_particles=200,bounds=10)
 swarm1.create_particles()
 swarm1.optimize()
 
